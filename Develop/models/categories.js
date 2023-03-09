2180c40f0d95a7bd915f6eff0944c0fd6e53eb02
byunn90 <kayhanturk90@Live.com>
2023-03-09 18:30:45 +1100

    added sever.js connection to controllers and main.handlebars/all

--- Develop/models/categories.js
+++ Develop/models/categories.js
@@ -1,6 +1,6 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection.js');
+const sequelize = require("../config/connection.js");
 
 class Category extends Model {}
 
@@ -23,7 +23,7 @@ Category.init(
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'category',
+    modelName: "category",
   }
 );
 
--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const path = require("path");
 const expressHandlebars = require("express-handlebars");
-// const routes = require("./routes");
+const mainRoutes = require("./controllers/main");
+const apiRoutes = require("./controllers/api/examples");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,10 +14,17 @@ app.use(express.urlencoded({ extended: true }));
 
 const hbs = expressHandlebars.create({});
 app.engine("handlebars", hbs.engine);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "handlebars");
+app.use(express.static(path.join(__dirname, "public")));
 
-// sync sequalize models to the database, then turn on the sever
+// Use the routes
+app.use("/", mainRoutes);
+app.use("/api", apiRoutes);
+
+// Sync sequelize models to the database, then turn on the server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}!`);
+    console.log(`Server listening on: http://localhost:${PORT}`);
   });
 });
