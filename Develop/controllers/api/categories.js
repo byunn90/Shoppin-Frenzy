2a1523e47ad0389c65669bcf140d856e42fe57ac
tcha0074 <tcha0074@student.monash.edu>
2023-03-12 18:43:40 +1100

    added routes and view for categories

--- Develop/controllers/api/categories.js
+++ Develop/controllers/api/categories.js
@@ -8,14 +8,30 @@ const { Category, Product } = require("../../models");
 // });
 
 router.get("/", async (req, res) => {
-  //   try {
-  console.log("Hi");
-  const category = await Category.findAll({
-    include: [{ model: Product }],
-  });
-  res.status(200).json(category);
-  //   } catch (error) {
-  //     res.status(500).send(error);
-  //   }
+  try {
+    console.log("Hi");
+    const category = await Category.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(category);
+
+  }catch (error) {
+       res.status(500).send(error);
+      }
 });
+
+router.get("/:name", async (req,res) => {
+  try{
+    console.log("Hi");
+    const category = await Category.findOne({
+      where: {
+        name: req.params.category_name,
+      },
+      include: [{model: Product}],
+    });
+    res.status(200).json(category);
+  }catch (error){
+    res.status(500).send(error);
+  }
+})
 module.exports = router;
--- Develop/controllers/api/index.js
+++ Develop/controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const products = require("./products");
+const category = require("./categories")
 
 router.use("/products", products);
+router.use("/category", category);
 
 module.exports = router;
