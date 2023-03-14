8e78c4e42c6c8d291152c49e657ea7aa0f759c3b
byunn90 <kayhanturk90@Live.com>
2023-03-14 20:21:23 +1100

    testing also added extra things

--- Develop/models/categories.js
+++ Develop/models/categories.js
@@ -30,5 +30,4 @@ Category.init(
     modelName: "category",
   }
 );
-
 module.exports = { Category };
---
+++ Develop/public/js/clothingCategories.js
@@ -0,0 +1,24 @@
+const tshirtsLink = document.getElementById("tshirts-link");
+tshirtsLink.addEventListener("click", function () {
+  window.location.href = "/t-shirts";
+});
+
+const hatsLink = document.getElementById("hats-link");
+hatsLink.addEventListener("click", function () {
+  window.location.href = "/hats";
+});
+
+const sportsLink = document.getElementById("sports-link");
+sportsLink.addEventListener("click", function () {
+  window.location.href = "/sports";
+});
+
+const boxingLink = document.getElementById("boxing-link");
+boxingLink.addEventListener("click", function () {
+  window.location.href = "/boxing";
+});
+
+const jeansLink = document.getElementById("jeans-link");
+jeansLink.addEventListener("click", function () {
+  window.location.href = "/jeans";
+});
