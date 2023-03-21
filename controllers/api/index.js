92ddbff175d12717cde3560d9c1066d62a43a8bf
byunn90 <kayhanturk90@Live.com>
2023-03-21 18:24:01 +1100

    test3

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,12 +1,12 @@
 const router = require("express").Router();
 const products = require("./products");
 const category = require("./categories");
-const categoryDropdown = require("./categoryDropdown");
+const dropDown = require("./categoryDropDown");
 
 const user = require("./userRoutes");
 router.use("/products", products);
 router.use("/category", category);
 router.use("/users", user);
-router.use("/categoryDropdown", categoryDropdown);
+router.use("/dropDown", dropDown);
 
 module.exports = router;
