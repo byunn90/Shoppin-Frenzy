b171d744e67128a52888585943e6c9433e8f82b5
byunn90 <kayhanturk90@Live.com>
2023-03-21 18:52:11 +1100

    dropdown

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,11 +2,11 @@ const router = require("express").Router();
 const products = require("./products");
 const category = require("./categories");
 const dropDown = require("./categoryDropDown");
-
 const user = require("./userRoutes");
+
 router.use("/products", products);
-router.use("/", category);
+router.use("/category", category);
 router.use("/users", user);
-router.use("/", dropDown);
+router.use("/dropDown", dropDown);
 
 module.exports = router;
