0d5ec5add5a618759fbb73ece2a65cf88610bbd7
byunn90 <kayhanturk90@Live.com>
2023-03-21 20:32:53 +1100

    test2

--- controllers/api/categoryDropDown.js
+++ controllers/api/categoryDropDown.js
@@ -1,13 +1,1 @@
-const router = require("express").Router();
-const { Category } = require("../../models");
 
-router.get("/", async (req, res) => {
-  try {
-    const categories = await Category.findOne();
-    res.status(200).json(categories);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-
-module.exports = router;
